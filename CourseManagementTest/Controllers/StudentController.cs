7f87eedfcbd1cd9132394b5db7dad9a417320122
ManthanThakor <thakormanthan849@gmail.com>
2025-01-02 14:06:15 +0530

    change

--- CourseManagementTest/Controllers/StudentController.cs
+++ CourseManagementTest/Controllers/StudentController.cs
@@ -36,8 +36,40 @@ namespace CourseManagementTest.Controllers
             {
                 return NotFound();
             }
-
             return View(student);
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateOrEdit(int id, [Bind("StudentId,Name,Email,Address,DOB")] Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                if (id == 0)
+                {
+                    _context.Add(student);
+                }
+                else
+                {
+                    _context.Update(student);
+                }
+
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(student);
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            var student = await _context.Students.FindAsync(id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+
+            _context.Students.Remove(student);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
         }
     }
 }
