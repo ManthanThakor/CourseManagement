2ea35e1c844bc7e07df9749a85c7a68e66734d4a
ManthanThakor <thakormanthan849@gmail.com>
2025-01-02 18:27:00 +0530

    change

---
+++ CourseManagementTest/Controllers/Enrollment.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CourseManagementTest.Controllers
+{
+    public class Enrollment : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- CourseManagementTest/obj/Debug/net8.0/CourseManagementTest.AssemblyInfo.cs
+++ CourseManagementTest/obj/Debug/net8.0/CourseManagementTest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CourseManagementTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e93ea830f2eb844c11828a730fdf0d6a4e50422")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f3f45932033184bcccb06384494c1bcc0a3153f")]
 [assembly: System.Reflection.AssemblyProductAttribute("CourseManagementTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CourseManagementTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
